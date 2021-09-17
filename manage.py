bb3168094cf3b1b76a1ab4c019b7a1c56a33d90f
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-09-17 18:26:26 +0300

    initializing various extnsions

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 from  flask_migrate import Migrate, MigrateCommand
-from app import create_app,db
+from app import create_app ,db
 from flask_script import Manager,Shell,Server
 from app.models import User
 
