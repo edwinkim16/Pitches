e8bd76d4155b07e048130287ee8c8107d89a9419
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-09-17 20:12:38 +0300

    modified navbar and views

---
+++ app/main/views.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from . import main
+
+from flask import render_template
+
+from ..models import User
+
+from .. import db
+app = Flask(__name__)
+
+
+# views
+@main.route("/")
+def index():
+   '''
+   title = "Pitch Perfect"
+   '''
+   title = 'Pitch Perfect'
+   
+
+   return render_template('index.html', title= title)
