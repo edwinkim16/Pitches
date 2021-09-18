59967f42afca59a9287491a63209a9f7da3a1208
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-09-18 19:05:34 +0300

    adding pitch,comment update profile form and pitch view

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from . import main
 from flask_login import login_required
-from flask import render_template
-
-from ..models import User
-
+from flask import render_template, redirect, url_for, abort, flash
+import datetime
+from ..models import User, Pitch, Comment
+from .forms import UpdateProfile, PitchForm , CommentForm
 from .. import db
 app = Flask(__name__)
 
@@ -19,3 +19,23 @@ def index():
    
 
    return render_template('index.html', title= title)
+
+@main.route('/pitch/new', methods=['GET','POST'])
+@login_required
+def new_pitch():
+    form = PitchForm()
+
+    if form.validate_on_submit():
+
+        title=form.title.data
+        content=form.content.data
+        category=form.category.data
+        pitch = Pitch(title=title, content=content,category=category)
+        # pitch.save_pitch(pitch)
+        db.session.add(pitch)
+        db.session.commit()
+
+        flash('Your pitch has been created!', 'success')
+        return redirect(url_for('main.index', id=pitch.id))
+
+    return render_template('new_pitch.html', title='New Post', pitch_form=form, post ='New Post')   
