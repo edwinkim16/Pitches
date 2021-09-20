89d38b1a3741a6bd5f9a6625f1ed8a3c4cf8c7f9
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-09-20 08:39:05 +0300

    removal of commented code

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD =os.environ.get("MAIL_PASSWORD")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
    
